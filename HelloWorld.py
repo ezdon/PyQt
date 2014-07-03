564c3b78f051f2b2e596df099a13a42cf35f8b5a
ezdon <ezdon123@gmail.com>
2014-07-03 12:36:34 +0100

    nm

--- HelloWorld.py
+++ HelloWorld.py
@@ -12,6 +12,7 @@ class MainWindow(QMainWindow):
         #create the layouts
         self.create_main_layout()
         self.create_hello_layout()
+        self.create_convert()
         #create central widget
         self.central_widget = QWidget()
         self.central_widget.setLayout(self.stacked_layout)
@@ -22,11 +23,13 @@ class MainWindow(QMainWindow):
         #create widgets
         self.text_box1 = QLineEdit()
         self.button = QPushButton("Submit")
+        self.label123 = QLabel("Enter your name into the box right away")
         #create an instance for QVBoxLayout()
         #Add widgets to layout (self.initial_layout)
         self.initial_layout = QVBoxLayout()
         self.initial_layout.addWidget(self.text_box1)
         self.initial_layout.addWidget(self.button)
+        self.initial_layout.addWidget(self.label123)
         #convert layout (self.initial_layout) to widget, so it can be a 'Central widget'
         self.initial_widget = QWidget()
         self.initial_widget.setLayout(self.initial_layout)
@@ -41,19 +44,33 @@ class MainWindow(QMainWindow):
         self.helloLabel = QLabel()
         self.button.clicked.connect(self.display_text)
         self.backButton = QPushButton("Back")
+        self.convertButton = QPushButton("Convert")
 
         self.layout1 = QVBoxLayout()
         
         self.layout1.addWidget(self.helloLabel)
         self.layout1.addWidget(self.backButton)
+        self.layout1.addWidget(self.convertButton)
 
         self.initial_widget1 = QWidget()
         self.initial_widget1.setLayout(self.layout1)
 
         self.stacked_layout.addWidget(self.initial_widget1)
         self.backButton.clicked.connect(self.back)
-    
+        self.convertButton.clicked.connect(self.convert)
 
+    def create_convert(self):
+        
+        self.note = QLabel("Name converting coming soon!")
+
+        self.layout3 = QVBoxLayout()
+        self.layout3.addWidget(self.note)
+
+        self.widget3 = QWidget()
+        self.widget3.setLayout(self.layout3)
+
+        self.stacked_layout.addWidget(self.widget3)
+        
     def switch_layout(self):
         self.stacked_layout.setCurrentIndex(1)
 
@@ -63,7 +80,8 @@ class MainWindow(QMainWindow):
     def display_text(self):
         name = self.text_box1.text()
         self.helloLabel.setText("Hello, {0}".format(name))
-        
+    def convert(self):
+        self.stacked_layout.setCurrentIndex(2)
 
 
 if __name__ == "__main__":
